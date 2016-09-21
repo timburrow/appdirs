9e29a059b7866bd9a508d4112014cae0e5969357
Timothy Burrow <timburrow@me.com>
2016-09-21 15:04:27 -0400

    added a blank appdir as a starting point

---
+++ blank/psglib/blank.c
@@ -0,0 +1,37 @@
+// Copyright (c) Your name
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+// 
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+ 
+
+#include <standard.h>
+#include <chempack.h>
+
+extern int dps_flag;
+
+
+pulsesequence()
+{
+  /* Setup up pulse sequence */
+  
+  /* BEGIN ACTUAL PULSE SEQUENCE */
+  status(A);
+  
+  status(B);
+  
+  status(C);
+
+}
